6d1984d9ba63ae8957e03de124bbece1cbe9c33a
jethrocadang <116892945+jethrocadang@users.noreply.github.com>
2024-11-13 22:02:36 +0800

    initial commit

---
+++ database/migrations/2024_11_13_134730_create_resumes_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('resumes', function (Blueprint $table) {
+            $table->id();
+            $table->string('file_name');
+            $table->string('file_url');
+            $table->timestamp('uploaded_at');
+            $table->string('parsed_text');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('resumes');
+    }
+};
